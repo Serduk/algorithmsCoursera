70ae89fdc0d2728a448857b6772cf4b69445cb5f
Serdiuk Serhii <serduksergey90@gmail.com>
2018-09-06 18:39:15 +0300

    add quick find

---
+++ src/quickFind/QuickFindUF.java
@@ -0,0 +1,26 @@
+package quickFind;
+
+public class QuickFindUF {
+    private int[] id;
+
+    public QuickFindUF(int N) {
+        id = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            id[i] = i;
+        }
+    }
+
+    public boolean isConnected(int a, int b) {
+        return id[a] == id[b];
+    }
+
+    public void union(int q, int p) {
+        int pid = id[p];
+        int qid = id[q];
+
+        for (int i = 0; i < id.length; i++) {
+            if (id[i] == pid) id[i] = qid;
+        }
+    }
+}
