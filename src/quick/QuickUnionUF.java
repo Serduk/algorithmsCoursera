4dec221034d674befa5c5e75d47f5008759bd19f
Serdiuk Serhii <serduksergey90@gmail.com>
2018-09-10 17:20:49 +0300

    minor optimize

--- src/quick/QuickUnionUF.java
+++ src/quick/QuickUnionUF.java
@@ -3,9 +3,9 @@ package quick;
 public class QuickUnionUF {
     private int[] id;
 
-    public QuickUnionUF(int N) {
-        id = new int[N];
-        for (int i = 0; i < N; i++) id[i] = i;
+    public QuickUnionUF(int n) {
+        id = new int[n];
+        for (int i = 0; i < n; i++) id[i] = i;
     }
 
     /*
