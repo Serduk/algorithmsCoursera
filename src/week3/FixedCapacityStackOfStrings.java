ab02b32febf8711c7f0b3aec1a62c439b7d3eb42
Serdiuk Serhii <serduksergey90@gmail.com>
2018-09-17 18:58:56 +0300

    add null assingment for object on "n" position

--- src/week3/FixedCapacityStackOfStrings.java
+++ src/week3/FixedCapacityStackOfStrings.java
@@ -17,6 +17,9 @@ public class FixedCapacityStackOfStrings {
     }
 
     public String pop() {
-        return s[--n];
+        String item = s[--n];
+        s[n] = null;
+
+        return item;
     }
 }
