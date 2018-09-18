1473336ef2d11409daebd967303e59afd3f91121
Serdiuk Serhii <serduksergey90@gmail.com>
2018-09-18 15:09:16 +0300

    add resizing for array

---
+++ src/week3/ResizingArrayStackOfStrings.java
@@ -0,0 +1,39 @@
+package week3;
+
+public class ResizingArrayStackOfStrings {
+    private String[] s;
+    private int n = 0;
+
+    public ResizingArrayStackOfStrings() {
+        s = new String[1];
+    }
+
+    public void push(String item) {
+        if (n == s.length) resize(2 * s.length);
+        s[n++] = item;
+    }
+
+    private void resize(int capacity) {
+        String[] copy = new String[capacity];
+
+        if (n >= 0) System.arraycopy(s, 0, copy, 0, n);
+
+        /*
+        * same as
+        * for (int i = 0; i < n; i++) {
+            copy[i] = s[i];
+        }
+        * */
+
+        s = copy;
+    }
+
+    public String pop() {
+        String item = s[--n];
+        s[n] = null;
+
+        if (n > 0 && n == s.length / 4) resize(s.length / 2);
+
+        return item;
+    }
+}
