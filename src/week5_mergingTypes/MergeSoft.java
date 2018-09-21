da85e593605317d276d4d226eac0d8a3a6b18ef9
Serdiuk Serhii <serduksergey90@gmail.com>
2018-09-21 18:37:04 +0300

    impelemt merge implementation

---
+++ src/week5_mergingTypes/MergeSoft.java
@@ -0,0 +1,44 @@
+package week5_mergingTypes;
+
+public class MergeSoft {
+    private static void merge(Comparable[] a, Comparable[] aux, int lo, int mid, int hi) {
+        assert  isSorted(a, lo, mid);
+        assert isSorted(a, mid+1, hi);
+
+        for (int k = lo; k <= hi; k++) {
+           aux[k] = a[k];
+        }
+
+        int i = lo, j = mid+1;
+
+        for (int k = lo; k <= hi; k++) {
+            if (i > mid) {
+                a[k] = aux[j++];
+            } else if (j > hi) {
+                a[k] = aux[i++];
+            } else if (less(aux[j], aux[i])) {
+                a[k] = aux[j++];
+            } else {
+                a[k] = aux[i++];
+            }
+        }
+    }
+
+    private static void sort(Comparable[] a, Comparable[] aux, int lo, int hi) {
+        if (hi <= lo) return;
+
+        int mid = lo + (hi - lo) / 2;
+
+        sort(a, aux, lo, mid);
+        sort(a, aux, mid + 1, hi);
+        merge(a, aux,lo, mid, hi);
+    }
+
+    private static boolean isSorted(Comparable[] a, int b, int c) {
+        return true;
+    }
+
+    private static boolean less(Comparable a, Comparable b) {
+        return true;
+    }
+}
