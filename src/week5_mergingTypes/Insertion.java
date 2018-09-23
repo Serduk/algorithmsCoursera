0528d621743f111ec5716ddd6fa2600df45b5ad5
Serdiuk Serhii <serduksergey90@gmail.com>
2018-09-24 00:34:24 +0300

    merge sort. insertion

---
+++ src/week5_mergingTypes/Insertion.java
@@ -0,0 +1,25 @@
+package week5_mergingTypes;
+
+
+public class Insertion {
+    public void sort(Comparable[] a) {
+        int n = a.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j > 0 && less(a[j], a[j-1]); j--) {
+                exch(a, j, j-1);
+            }
+        }
+    }
+
+    private static boolean less(Object b, Object c) {
+//        return a.compare(b, c) < 0;
+        return true;
+    }
+
+    private static void exch(Object[] a, int j, int b) {
+        Object swap = a[j];
+        a[j] = b;
+        a[b] = swap;
+    }
+}
