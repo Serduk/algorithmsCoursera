18115a0d04f2b4d040210473e719e5de4401696f
Serdiuk Serhii <serduksergey90@gmail.com>
2018-09-24 15:23:42 +0300

    add System Sort body

--- src/week5_quickSortTypes/SystemSorts.java
+++ src/week5_quickSortTypes/SystemSorts.java
@@ -1,4 +1,20 @@
 package week5_quickSortTypes;
 
+import java.util.Arrays;
+
+/**
+ * System sorts:
+ * https://www.coursera.org/learn/algorithms-part1/lecture/QBNZ7/system-sorts
+ * */
 public class SystemSorts {
+    public static void main(String[] args) {
+        String[] a = args;
+
+        int n = a.length;
+
+        Arrays.sort(a);
+        for (int i = 0; i < n; i++) {
+            System.out.println(a[i]);
+        }
+    }
 }
