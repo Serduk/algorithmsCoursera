cb58a37b93e5db67c5effaee8a71cc01d140329c
Serdiuk Serhii <serduksergey90@gmail.com>
2018-09-24 15:30:22 +0300

    simplify SYstemSorts. add description

--- src/week5_quickSortTypes/SystemSorts.java
+++ src/week5_quickSortTypes/SystemSorts.java
@@ -5,16 +5,25 @@ import java.util.Arrays;
 /**
  * System sorts:
  * https://www.coursera.org/learn/algorithms-part1/lecture/QBNZ7/system-sorts
+ *
+ * Correct
+ * The Java API for \verb#Arrays.sort()#Arrays.sort()
+ * for reference types requires that it is stable and guarantees
+ * n \log nnlogn performance. Neither of these are properties of standard quicksort.
+ *
+ * Quicksort uses less memory and is faster
+ * in practice on typical inputs (and is typically used by
+ * \verb#Arrays.sort()#Arrays.sort() when sorting primitive types,
+ * where stability is not relevant).
+ *
  * */
 public class SystemSorts {
     public static void main(String[] args) {
-        String[] a = args;
+        int n = args.length;
 
-        int n = a.length;
-
-        Arrays.sort(a);
-        for (int i = 0; i < n; i++) {
-            System.out.println(a[i]);
+        Arrays.sort(args);
+        for (String anA : args) {
+            System.out.println(anA);
         }
     }
 }
