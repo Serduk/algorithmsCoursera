db31d6f4c71a34645608a2c4791a2228043ac9eb
Serdiuk Serhii <serduksergey90@gmail.com>
2018-10-02 23:49:42 +0300

    implemente deletion

--- src/symbolTables/Node.java
+++ src/symbolTables/Node.java
@@ -94,4 +94,29 @@ public class Node<Key, Value> implements Comparable {
         if (node == null) return 0;
         else return node.count;
     }
+
+    public void delete(Key key) {
+        root = delete(root, key);
+    }
+
+    public Node delete(Node node, Key key) {
+        if (node == null) return null;
+
+//        int cmp = key.compareTo(key);
+        int cmp = 1;
+
+//        if (cmp < 0) node.left() = delete(node.left(), key);
+//        else if (cmp > 0) node.right() = delete(node.right(), key);
+//        else {
+//            if (node.right() == null) return node.left();
+//            if (node.left() == null) return node.right();
+
+//            Node t = node;
+//            t = min(t.right());
+//            t.right() = delete(t.right());
+//            node.left() = t.left();
+//        }
+        node.count = size(node.left()) + size(node.right()) + 1;
+        return node;
+    }
 }
